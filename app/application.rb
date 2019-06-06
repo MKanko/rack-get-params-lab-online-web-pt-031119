c105e4e056d6fb19056a3d0c17ccfabe3c89d874
Mark Kanko <mkanko66@gmail.com>
2019-06-06 00:11:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,22 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path.match(/cart/)
+      if @@cart.count == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}"
+        end 
+      end 
+
+    elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
